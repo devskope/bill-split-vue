81ef715da054da20cfadfd01170b7cca61b6f4a7
devskope <ope.oguntoye@protonmail.com>
2020-01-22 17:13:35 +0100

    fix home path, add bill list route [#8]

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/home/Home.vue';
 import { store } from '@/store';
 
 Vue.use(VueRouter);
-
+const { auth } = store.state;
 const routes = [
   {
     path: '/',
@@ -17,16 +17,21 @@ const routes = [
     component: Home,
     props: { auth: true },
     beforeEnter: (to, from, next) => {
-      setTimeout(
-        () => (store.state.auth.loggedIn ? next('/bills/list') : next()),
-        3,
-      );
+      setTimeout(() => (auth.loggedIn ? next('/bills/list') : next()), 3);
     },
   },
   {
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/About'),
+  },
+  {
+    path: '/bills/list',
+    name: 'billList',
+    component: () => import(/* webpackChunkName: "billList" */ '../views/bills/BillList'),
+    beforeEnter: (to, from, next) => {
+      setTimeout(() => (auth.loggedIn ? next() : next('/auth')), 3);
+    },
   },
 ];
 
